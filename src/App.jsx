daa057a8db90458f9daba8a8bb8a54a31e9b20e1
aaaaiii11 <2277134573@qq.com>
2025-05-10 00:35:40 +0800

    newbook_3

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,41 @@
 import { Table, Button, Flex, Form, Popover, Input, Modal, Space } from 'antd';
 import { useForm } from 'antd/es/form/Form';
-import { useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Books() {
   const [books, setBooks] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [searchbook,setSearchbook]=useState('');
-  const [pagination,setPagination]=useState({
-    current:1,
-    pageSize:10,
-    total:0,
-    sortBy:'createdAt',
-    pageSizeOptions:[10,20,50,100],
-    showSizeChanger:true
+  const [searchbook, setSearchbook] = useState('');
+  const [pagination, setPagination] = useState({
+    current: 1,
+    pageSize: 10,
+    total: 0,
+    sortBy: 'createdAt',
+    order:'desc',
+    pageSizeOptions: [10, 20, 50, 100],
+    showSizeChanger: true
   })
   const url = 'http://lesson.creaverse.cc/books';
-  
-  
+
+
   //显示所有图书
   async function fetchshowbooks() {
-    const params= new URLSearchParams({
+    const params = new URLSearchParams({
       page: pagination.current.toString(),
       limit: pagination.pageSize.toString(),
-      sortBy: 'title',
-      order: 'asc',
-      
+      sortBy: pagination.sortBy,
+      order: pagination.order,
     })
-    if(searchbook){
-      params.append('search',searchbook);
+    if (searchbook) {
+      params.append('search', searchbook);
     }
     setLoading(true);
     await fetch(`http://lesson.creaverse.cc/books?${params}`)
-    .then(response => response.json())
+      .then(response => response.json())
       .then(data => {
         setPagination({
           ...pagination,
-          total:`${data.pagination.totalItems}`
+          total: `${data.pagination.totalItems}`
         }
         )
         return (setBooks(data.data.map(e => ({
@@ -49,25 +49,28 @@ export default function Books() {
       .catch(err => (console.log(err.message)))
     setLoading(false);
   }
-//页码发生变化的时候
-  const handleOnchange=(pagination)=>{
+  //页面要求变化的时候
+  const handleOnchange = (pagination,filters, sorter) => {
+    const sortField = sorter.field || 'createdAt';
+    const sortOrder = sorter.order ? (sorter.order === 'ascend' ? 'asc' : 'desc') : 'desc';
+    
     setPagination({
-     ...pagination
+      ...pagination,
+      sortBy: sortField,
+      order: sortOrder
     })
   }
 
   useEffect(() => {
     fetchshowbooks();
-  }, [pagination.current,searchbook,pagination.pageSize])
+  }, [pagination.current, searchbook, pagination.pageSize,pagination.order,pagination.sortBy])
 
   const columns = [
     {
       title: 'id',
       dataIndex: 'id',
       key: 'id',
-      sorter:{
-        compare:(a,b)=>a.id - b.id,
-      }
+      sorter: true,
     },
     {
       title: '图书',
@@ -82,7 +85,8 @@ export default function Books() {
     {
       title: '创造日期',
       dataIndex: 'createdAt',
-      key: 'createdAt'
+      key: 'createdAt',
+      sorter:true,
     },
     {
       title: '上次更新',
@@ -108,7 +112,7 @@ export default function Books() {
         <Addbook fetchshowbooks={fetchshowbooks} url={url} />
         <SearchBook setSearchbook={setSearchbook} setPagination={setPagination} pagination={pagination}></SearchBook>
       </Space>
-      <Table loading={loading} dataSource={books} columns={columns} pagination={pagination} onChange={handleOnchange}/>
+      <Table loading={loading} dataSource={books} columns={columns} pagination={pagination} onChange={handleOnchange} />
     </div>
   )
 }
@@ -116,6 +120,7 @@ export default function Books() {
 const Addbook = ({ fetchshowbooks, url }) => {
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
+  const [buttonstate, setButtonstate] = useState(true);
   const [newbook, setNewBook] = useState({ title: '', author: '' });
   const [form] = useForm();
 
@@ -137,7 +142,11 @@ const Addbook = ({ fetchshowbooks, url }) => {
   const onValuesChange = (changedValues, allValues) => {
     setNewBook(allValues)
   }
-
+  //检验添加的信息
+  useEffect(() => {
+    form.validateFields({ validateOnly: true }).then(() => setButtonstate(false))
+      .catch(() => setButtonstate(true))
+  }, [newbook])
   return (
     <>
       <Button onClick={() => { setOpen(true) }}>添加图书</Button>
@@ -152,16 +161,18 @@ const Addbook = ({ fetchshowbooks, url }) => {
           handleCancel();
         }}
         confirmLoading={confirmLoading}
-        destroyOnClose={true}
+        okButtonProps={{ disabled: buttonstate }}
       >
-        <Form name={form}
+        <Form
+          name='addbook'
           labelCol={{ span: 4 }}
           onValuesChange={onValuesChange}
+          form={form}
         >
-          <Form.Item label='title' name='title'>
+          <Form.Item label='title' name='title' rules={[{ required: true }]}>
             <Input disabled={confirmLoading} />
           </Form.Item>
-          <Form.Item label='author' name='author'>
+          <Form.Item label='author' name='author' rules={[{ required: true }]}>
             <Input disabled={confirmLoading} />
           </Form.Item>
         </Form>
@@ -212,8 +223,9 @@ const DelbookPopover = ({ fetchshowbooks, url, bookid }) => {
 const ModifyBook = ({ fetchshowbooks, book, url }) => {
   const [open, setOpen] = useState(false);
   const [upbook, setUpbook] = useState({ title: '', author: '' });
+  const [okbutton, setOkbutton] = useState(true);
   const [loading, setLoading] = useState(false);
-  const [modiform] = useForm('');
+  const [modiform] = useForm();
 
   const modifyBook = async () => {
     await fetch(`${url}/${book.id}`, {
@@ -223,7 +235,11 @@ const ModifyBook = ({ fetchshowbooks, book, url }) => {
     }).then(response => response.json()).then(response => { console.log(`修改成功${JSON.stringify(response)}`) })
     fetchshowbooks();
   }
-
+  //检验数据是否为空
+  useEffect(() => {
+    modiform.validateFields({ validateOnly: true }).then(e=>console.log(e))
+      .then(() => setOkbutton(false)).catch(() => setOkbutton(true))
+  }, [upbook])
   const onCancel = () => {
     setOpen(false);
   }
@@ -246,16 +262,18 @@ const ModifyBook = ({ fetchshowbooks, book, url }) => {
           setUpbook({ title: '', author: '' });
         }}
         destroyOnClose={true}
+        okButtonProps={{ disabled: okbutton }}
       >
-        <Form name={modiform}
+        <Form name='modifybook'
           onValuesChange={onValuesChange}
           labelCol={{ span: 4 }}
+          form={modiform}
         >
           <Form.Item label='title' name='title'>
-            <Input disabled={loading} defaultValue={book.title}></Input>
+            <Input disabled={loading} defaultValue={book.title} rules={[{ required: true }]}></Input>
           </Form.Item>
           <Form.Item label='author' name='author'>
-            <Input disabled={loading} defaultValue={book.author}></Input>
+            <Input disabled={loading} defaultValue={book.author} rules={[{ required: true }]}></Input>
           </Form.Item>
         </Form>
       </Modal>
@@ -263,15 +281,15 @@ const ModifyBook = ({ fetchshowbooks, book, url }) => {
   )
 }
 //搜索组件 
-const SearchBook = ({setSearchbook,setPagination,pagination}) => {
+const SearchBook = ({ setSearchbook, setPagination, pagination }) => {
   const { Search } = Input;
   const findBook = (e) => {
     setPagination({
       ...pagination,
-      current:'1',
+      current: '1',
     })
     setSearchbook(e);
-    }
+  }
   return (
     <>
       <Search onSearch={findBook} style={{ width: 200 }}></Search>
